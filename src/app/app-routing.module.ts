8711285a7cc6787cb8f1774643165790c069cd84
udith-wiratunga <udith.wiratunga@gmail.com>
2019-04-09 15:28:04 +0530

    6 files modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,11 @@ import { HerosComponent } from './heros/heros.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [ { path: 'heros', component: HerosComponent},
-                         { path: 'dashboard', component: DashboardComponent}
+
+const routes: Routes = [  { path:'',redirectTo:'./dashboard',pathMatch:'full'},
+                          { path: 'heros', component: HerosComponent},
+                          { path: 'dashboard', component: DashboardComponent},
+                          { path: 'detail/:id', component:HeroDetailComponent}
                         ];
 
 @NgModule({
